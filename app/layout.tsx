7e5e4e60acb9cd41494fcd42d9482ae2bd59d2b2
jeansamist <jeansamist@gmail.com>
2025-04-13 06:25:02 +0100

    refactor(styles): replace Geist font with Cairo for consistency
    The Geist font family has been replaced with Cairo to ensure a more consistent and unified design across the application. This change includes updating the font variables in the global CSS and adjusting the layout component to use the new font. The Cairo font offers better readability and aligns with the desired aesthetic.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Cairo } from "next/font/google";
 import "./globals.css";
 import { AppLayout } from "@/components/layouts/app-layout";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const font = Cairo({
+  variable: "--font-cairo-sans",
+  weight: "variable",
   subsets: ["latin"],
 });
 
@@ -25,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${font.variable} antialiased`}>
         <AppLayout>{children}</AppLayout>
       </body>
     </html>
