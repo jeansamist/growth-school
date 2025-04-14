ecdaaf7c79c07b54cf41929b4b2e97c53bcee009
jeansamist <jeansamist@gmail.com>
2025-04-14 08:33:24 +0100

    feat(book/[id]): implement book details page with dynamic data fetching
    Add functionality to fetch and display book details, including tags, category, and files, using Prisma. Include breadcrumb navigation and conditional image rendering based on the book category. This enhances the user experience by providing detailed information about each book.

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -1,5 +1,50 @@
+import prisma from "@/lib/prisma";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-export default async function page() {
-  return <div>page</div>;
+export default async function page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id: bookId } = await params;
+  const TAGS = await prisma.tag.findMany();
+  const book = await prisma.item.findUnique({
+    where: { id: Number(bookId) },
+    include: {
+      tags: true,
+      category: true,
+      files: true,
+    },
+  });
+  return (
+    <div className="container  px-6 lg:px-12 mx-auto space-y-12 pt-6">
+      <div className="font-bold">
+        <Link className="text-primary" href={"/"}>
+          Accueil /
+        </Link>
+        <Link
+          className="text-primary"
+          href={"/book/?category=" + book?.categoryId.toString()}
+        >
+          {book?.category.name} /
+        </Link>
+        <span className="opacity-70">{book?.title} /</span>
+      </div>
+      <div className="flex gap-6 flex-col xl:flex-row">
+        {book?.category.id === 1 && (
+          <div>
+            <Image
+              src={book.cover}
+              width={600}
+              height={1000}
+              alt={book?.title}
+              className="!h-auto aspect-video object-cover rounded-xl"
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
